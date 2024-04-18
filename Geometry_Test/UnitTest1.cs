88a8c3a9cc171a69f0d53b648ec85f9e8a96454e
A00304901 <163214232+A00304901@users.noreply.github.com>
2024-04-18 21:35:44 +0000

    wohoo, completed

--- Geometry_Test/UnitTest1.cs
+++ Geometry_Test/UnitTest1.cs
@@ -9,89 +9,126 @@ public class UnitTest1
     public void TestMethod1()
     {
         double sideLength = 5;
-            var Square = new Square(sideLength);
-
-            // Act
-            double result = Square.CalcArea();
-
-            // Assert
-            Assert.AreEqual(25, result);
+        var Square = new Square(sideLength);
+        double result = Square.CalcArea();
+        Assert.AreEqual(25, result);
     }
 
     [TestMethod]
     public void TestSquarePerimeter()
     {
-        // Arrange
         double sideLength = 5;
         var square = new Square(sideLength);
-
-        // Act
         double result = square.CalcPerimeter();
-
-        // Assert
         Assert.AreEqual(20, result);
-        }
+    }
+    [TestMethod]
+    public void TestSquareArea()
+    {
+        double sideLength = 5;
+        var square = new Square(sideLength);
+        double result = square.CalcArea();
+        Assert.AreEqual(25, result);
+    }
 
     [TestMethod]
-    public void TestRectangleArea()
+    public void TestSquarePerimeter2()
     {
-        // Arrange
+        double sideLength = 5;
+        var square = new Square(sideLength);
+        double result = square.CalcPerimeter();
+        Assert.AreEqual(20, result);
+    }
+
+
+    [TestMethod]
+    public void TestRectangleArea2()
+    {
         double length = 5;
         double width = 3;
         var rectangle = new Rectangle(length, width);
-
-        // Act
         double result = rectangle.CalcArea();
-
-        // Assert
         Assert.AreEqual(15, result);
     }
 
-        [TestMethod]
-        public void TestRectanglePerimeter()
-        {
-            // Arrange
-            double length = 5;
-            double width = 3;
-            var rectangle = new Rectangle(length, width);
+    [TestMethod]
+    public void TestRectanglePerimeter2()
+    {
+        double length = 5;
+        double width = 3;
+        var rectangle = new Rectangle(length, width);
+        double result = rectangle.CalcPerimeter();
+        Assert.AreEqual(16, result);
+    }
+    [TestMethod]
+    public void TestRectangleArea()
+    {
+        double length = 5;
+        double width = 3;
+        var rectangle = new Rectangle(length, width);
+        double result = rectangle.CalcArea();
+        Assert.AreEqual(15, result);
+    }
 
-            // Act
-            double result = rectangle.CalcPerimeter();
+    [TestMethod]
+    public void TestRectanglePerimeter()
+    {
+        double length = 5;
+        double width = 3;
+        var rectangle = new Rectangle(length, width);
+        double result = rectangle.CalcPerimeter();
+        Assert.AreEqual(16, result);
+    }
 
-            // Assert
-            Assert.AreEqual(16, result);
-        }
 
-        [TestMethod]
-        public void TestTriangleArea()
-        {
-            // Arrange
-            double side1 = 5;
-            double side2 = 6;
-            double side3 = 7;
-            var triangle = new Triangle(side1, side2, side3);
+        
+    [TestMethod]
+    public void TestTrianglePerimeter1()
+    {
+        double side1 = 5;
+        double side2 = 6;
+        double side3 = 7;
+        var triangle = new Triangle(side1, side2, side3);
+        double result = triangle.PerimeterCalculate();
+        Assert.AreEqual(18, result);
+    }
+    [TestMethod]
+        public void TestTrianglePerimeter2()
+    {
+        double side1 = 3;
+        double side2 = 4;
+        double side3 = 5;
+        var triangle = new Triangle(side1, side2, side3);
+        double result = triangle.PerimeterCalculate();
+        Assert.AreEqual(12, result);
+    }
+    [TestMethod]
+    public void TestTriangleArea3()
+    {
+        double side = 5;
+        var triangle = new Triangle(side, side, side);
+        double result = triangle.AreaCalculate();
+        Assert.AreEqual(10.825317547305485, result, 0.0001);
+    }
 
-            // Act
-            double result = triangle.AreaCalculate();
+    [TestMethod]
+    public void TestTrianglePerimeter4()
+    {
+        double side = 5;
+        var triangle = new Triangle(side, side, side);
+        double result = triangle.PerimeterCalculate();
+        Assert.AreEqual(15, result);
+    }
 
-            // Assert
-            Assert.AreEqual(14.696938456699069, result, 0.0001);
-        }
-
-        [TestMethod]
-        public void TestTrianglePerimeter()
-        {
-            // Arrange
-            double side1 = 5;
-            double side2 = 6;
-            double side3 = 7;
-            var triangle = new Triangle(side1, side2, side3);
-
-            // Act
-            double result = triangle.PerimeterCalculate();
-
-            // Assert
-            Assert.AreEqual(18, result);
-        }
+    [TestMethod]
+    public void TestTriangleArea5()
+    {
+        double side1 = 3;
+        double side2 = 4;
+        double side3 = 5;
+        var triangle = new Triangle(side1, side2, side3);
+        double result = triangle.AreaCalculate();
+        Assert.AreEqual(6, result);
+    }
 }
     
